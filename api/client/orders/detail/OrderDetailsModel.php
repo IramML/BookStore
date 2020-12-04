6f0e9db57dedffc4049b3a35973d4f3e37d02ec2
IramML <irammartinezlamas@gmail.com>
2020-12-04 11:36:22 -0600

    Fix order category name

--- api/client/orders/detail/OrderDetailsModel.php
+++ api/client/orders/detail/OrderDetailsModel.php
@@ -9,9 +9,13 @@ class OrderDetailsModel extends Model {
     
     function getOrderDetails($data) {
         try {
-            $query = $this->db->connect()->query("SELECT client_user_order.id as order_id, client_user_order.is_pdf, client_user_order.datetime as created_date, book.*  
+            $query = $this->db->connect()->query("SELECT client_user_order.id as order_id, 
+                client_user_order.is_pdf, client_user_order.datetime as created_date, 
+                book.*,
+                book_category.name as category_name
                 FROM client_user_order 
                 INNER JOIN book ON client_user_order.book_id = book.id
+                INNER JOIN book_category ON book.category_id = book_category.id
                 WHERE client_user_order.user_id = '$data[user_id]' AND client_user_order.id = '$data[order_id]'");
             $res = $query->fetch(PDO::FETCH_ASSOC);
             return $res;
--- api/client/orders/get/GetOrdersModel.php
+++ api/client/orders/get/GetOrdersModel.php
@@ -10,9 +10,13 @@ class GetOrdersModel extends Model {
     function getOrdersByUserID($userID) {
         $items = [];
         try {
-            $query = $this->db->connect()->query("SELECT client_user_order.id as order_id, client_user_order.is_pdf, client_user_order.datetime as created_date, book.*  
+            $query = $this->db->connect()->query("SELECT client_user_order.id as order_id, 
+                client_user_order.is_pdf, client_user_order.datetime as created_date, 
+                book.*,
+                book_category.name as category_name
                 FROM client_user_order 
                 INNER JOIN book ON client_user_order.book_id = book.id
+                INNER JOIN book_category ON book.category_id = book_category.id
                 WHERE client_user_order.user_id = '$userID'");
             $items = $query->fetchAll(PDO::FETCH_ASSOC);
             return $items;
